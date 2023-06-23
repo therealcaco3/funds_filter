a810e9590608890501c07d094d5b113d7eb9ecf6
therealcaco3 <91302596+therealcaco3@users.noreply.github.com>
2023-06-24 00:54:49 +0800

    Update funds_filter_0.3.0.py

--- funds_filter_0.3.0.py
+++ funds_filter_0.3.0.py
@@ -7,11 +7,11 @@ import streamlit as st
 from st_aggrid import AgGrid
 
 # Convert and extract data from the selected worksheet
-def data_organize(file_path, sheet_name):
+def data_organize(file_path, selected_sheet_name):
     # Check the selected sheet
-    if sheet_name == '境內(TWD計價) -  ':
+    if selected_sheet_name == '境內(TWD計價) -  ':
         # Load the Domestic worksheet into a DataFrame, excluding columns F(5) and G(6) 
-        df = pd.read_excel(file_path, sheet_name, header=None,
+        df = pd.read_excel(file_path, sheet_name=selected_sheet_name, header=None,
                             skiprows=11,    # starts from A12, so skip the first 11 rows
                             usecols=lambda x: x not in [5, 6])   # column indices in pandas are zero-based, so F = 5, G = 6.
     
@@ -31,9 +31,9 @@ def data_organize(file_path, sheet_name):
         last_row_index = df['理柏 ID'].last_valid_index()
         data = df.loc[:last_row_index]
 
-    elif sheet_name == '境外(USD計價) -  ':
+    elif selected_sheet_name == '境外(USD計價) -  ':
         # Load the Overseas worksheet into a DataFrame, excluding columns A(0)
-        df = pd.read_excel(file_path, sheet_name=sheet_name, header=None,
+        df = pd.read_excel(file_path, sheet_name=selected_sheet_name, header=None,
                         skiprows=11,    # starts from A12, so skip the first 11 rows
                         usecols=lambda x: x not in [0])   # column indices in pandas are zero-based
 
@@ -53,7 +53,7 @@ def data_organize(file_path, sheet_name):
         # Determine the last row based on the presence of data in column B
         last_row_index = df['理柏 ID'].last_valid_index()
         data = df.loc[:last_row_index]
-
+    
     return data
 
 
@@ -75,20 +75,20 @@ def funds_filter(sheet_name, data, classification, figures, thresholds):
     filtered_figures = pd.concat([names, filtered_figures], axis=1)
 
     # Convert thresholds from percentage to decimal values
-    thresholds_divide = [float(t) / 100 for t in thresholds]
+    thresholds = [float(t) / 100 for t in thresholds]
 
     # Apply the user-defined thresholds to filter the securities
     filtered_results = filtered_figures.loc[
-                        (filtered_figures[figures[0]]) &
-                        (filtered_figures[figures[0]] <= filtered_figures[figures[0]].quantile(thresholds_divide[0]))]
+                        (filtered_figures[figures[0]].notna()) &
+                        (filtered_figures[figures[0]] <= filtered_figures[figures[0]].quantile(thresholds[0]))]
     for i in range(1, len(figures)):
         filtered_results = filtered_results.loc[
-                        (filtered_results[figures[i]]) &
-                        (filtered_results[figures[i]] <= filtered_results[figures[i]].quantile(thresholds_divide[i]))]
-        
+                        (filtered_results[figures[i]].notna()) &
+                        (filtered_results[figures[i]] <= filtered_results[figures[i]].quantile(thresholds[i]))]
+    
     # converting '排名' columns from float to int
-    rank_cols = [c for c in filtered_results.columns if '排名' in c]
-    filtered_results[rank_cols] = filtered_results[rank_cols].apply(pd.to_numeric, downcast='integer', errors='ignore')    
+    rank_cols = [r for r in filtered_results.columns if '排名' in r]
+    filtered_results[rank_cols] = filtered_results[rank_cols].apply(pd.to_numeric, downcast='integer', errors='ignore')
 
     return filtered_results
 
@@ -103,15 +103,17 @@ st.set_page_config(
 
 st.title('基金篩選器')
 
+
 # User input using Streamlit
 st.markdown("#### 請上傳檔案：")
 uploaded_file = st.file_uploader("上傳 .xlsx 檔案", type=".xlsx")
+
 if uploaded_file is not None:
     # Read the uploaded file
     file_content = uploaded_file.read()
     wb = pd.read_excel(io.BytesIO(file_content), sheet_name=None)
-    worksheet_names = wb.keys()
-    sheet_input = st.selectbox("請選擇工作表：", list(worksheet_names))
+    sheet_names = wb.keys()
+    sheet_input = st.selectbox("請選擇工作表：", list(sheet_names))
     
     # Perform data processing and filtering based on the selected sheet and other inputs
     classification = st.text_input("請選擇基金分類：")
@@ -127,23 +129,18 @@ if uploaded_file is not None:
     rank_5Y = st.number_input(label = '請輸入5Y報酬率排名百分比：', value = 100, max_value = 100, min_value = 0)
     rank_10Y = st.number_input(label = '請輸入10Y報酬率排名百分比：', value = 100, max_value = 100, min_value = 0)
 
+    selected_sheet_name = '境外(USD計價) -  ' if sheet_input == '境外(USD計價) -  ' else '境內(TWD計價) -  '
     figures = ['1M排名', '3M排名', '6M排名', '1Y排名', '2Y排名', '3Y排名', '5Y排名', '10Y排名']
     thresholds = [rank_1M, rank_3M, rank_6M, rank_1Y, rank_2Y, rank_3Y, rank_5Y, rank_10Y]
 
     # Execute the filter functions
-    data = data_organize(uploaded_file, sheet_input)
-    result = funds_filter(sheet_input, data, classification, figures, thresholds)
+    data = data_organize(uploaded_file, selected_sheet_name)
+    result = funds_filter(selected_sheet_name, data, classification, figures, thresholds)
 
     # Display the filtered results
     st.markdown("#### 篩選結果及其排名(可按住Shift鍵+滾輪滑動表格)")
     # Display the merged dataframe with horizontal scrollbar
     AgGrid(result)
-    st.table(result)
-
-    # Display the filtered results
-    st.markdown("#### 篩選結果及其排名(st.table ver)(可按住Shift鍵+滾輪滑動表格)")
-    # Display the merged dataframe with horizontal scrollbar
-    st.table(result)
 
     # Merge the two dataframes based on the "名稱" column
     merged_data = pd.merge(result['名稱'], data, on='名稱')
